90a7578f87aa0ef79fd83b1ece033d2ebad3d7ac
timo1511 <tim.makori@gmail.com>
2022-04-07 15:49:14 -0700

    Craeted the 6-size file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ */
 
 int main(void)
 {
@@ -18,6 +18,7 @@ int main(void)
 		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 		return (0);
 }
 
