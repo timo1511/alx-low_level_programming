f266f34686e51efa76af86574e1d04aab52ed04f
timo1511 <tim.makori@gmail.com>
2022-04-07 15:35:47 -0700

    Craeted the 6-size file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
  *    * Return: Always 0 (Success)
  *    
  *    
- *    */
+ *    
+*/
 int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
