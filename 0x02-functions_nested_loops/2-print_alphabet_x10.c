cc34ea30d40513e7fe039d1b692c39270d5cd0c9
timo1511 <tim.makori@gmail.com>
2022-04-12 14:29:43 -0700

    Created the 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return:void
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			-putchar(c);
+			c++;
+		}
+		-putchar('\n');
+		i++;
+	}
+}
