151541d4f89ad82c45189824be85aee1bc999ec5
timo1511 <tim.makori@gmail.com>
2022-04-13 17:26:55 -0700

    Added the 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 			       	_putchar(' ');
 				_putchar('0' + rep);
 			}
-			else if (rep < 100);
+			else if (rep < 100)
 			{
 				_putchar(' ');
 				_putchar('0' + rep / 10);
