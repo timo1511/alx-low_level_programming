6d03786b5218db933ca74a9ce0d95c85303c1baf
timo1511 <tim.makori@gmail.com>
2022-04-20 15:15:08 -0700

    Created the 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+*/
+
+void swap_int(int *a, int *b)
+{
+	int changed;
+
+	changed = *a;
+	*a = *b;
+	*b = changed;
+}
