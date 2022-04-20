379eb2e336617271e54dc6cd280ad8bbd21ff2a6
timo1511 <tim.makori@gmail.com>
2022-04-20 15:43:48 -0700

    Created the 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in  reverse
+ *
+ * @s: Input string
+ *
+*/
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index--)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
