a0d106c8b30f970fb3a151877f163de826c11e74
timo1511 <tim.makori@gmail.com>
2022-04-27 14:36:58 -0700

    Created the memset file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant of "n" bytes
+ * @s: array of set
+ * @b: what to set it to
+ * @n: number of bytes
+ * Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		s[x] = b;
+	return (s);
+}
