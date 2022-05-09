6fdfa845b14cad796740dea019fa9fed8c6b0272
timo1511 <tim.makori@gmail.com>
2022-05-09 03:33:16 -0700

    Corrected the 1st file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc : variable to count number of parameters
+ * @argv : variable to count number of items in array
+ * Return: Always 0 (Success)
+*/
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	 _putchar ("This program was called with \"%s\".\n",argv[0]);
+	 if (argc > 0)
+		 printf("%s\n", argv[0]);
 	 return (0);
 }
