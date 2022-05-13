b3e419389c5b1a3ede5990ed7605ede7d3eefe5b
timo1511 <tim.makori@gmail.com>
2022-05-12 18:57:04 -0700

    Created the dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: The attributes of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
